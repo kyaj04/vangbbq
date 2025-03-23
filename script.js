e43d58a6c98a2e3e5ef80fc07a653ef72868e96a
Ky Yang <kyang44@wgu.edu>
2025-03-22 23:59:30 -0700

    add isvalid

--- script.js
+++ script.js
@@ -198,6 +198,14 @@ function sendOrdersToLaptop() {
     .then((response) => response.text())
     .then((data) => console.log("Response from server:", data))
     .catch((error) => console.error("Error:", error));
+
+
+}
+
+// Function to check if the IP address format is valid
+function isValidIP(ip) {
+  const regex = /^(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$/;
+  return regex.test(ip);
 }
 
 function loadOrdersFromLocalStorage() {
